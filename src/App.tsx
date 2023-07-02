90e7133f3947f9e65de7b23719d82e9fde5c79e4
Dmi Pro <dprotascik@thetrask.com>
2023-07-02 17:50:52 +0200

    using hookes

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,31 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import "./styles.css"
-let productName = "test2"
-let data: any;
 
-fetch('https://dummyjson.com/products')
-  .then(res => res.json())
-  .then(res => {
-    data = res;
-   
-    productHandler(data);
-  });
-
-function productHandler(data: { products: any[] }) {
-  //console.log(data);
-  data.products.forEach((element: any) => {
-    
-    console.log(element.title);
-    setProduct(element.title);
-
-  });
-  
-}
 
 export default function App(){
 
-  const [productName, setProduct] = useState("")
-  //setProduct("test");
+  const [products, setProducts] = useState([]);
+
+  useEffect(()=> {
+    fetch('https://dummyjson.com/products')
+    .then(res => res.json())
+    .then(res => {
+      console.log(res.products);
+      setProducts(res.products);
+     
+    });
+  ``
+  },[])
+  
+
 
   return(
     <>
-   <table>
-  <tr>
-    <td>Product1</td>
-    <td>{productName}</td>
-    <td>Product2</td>
-  </tr>
-</table>
-  
-  </> 
+    <div className="product-container">
+      {products.map((item, index) => (
+        <div className="product-item" key={item.id}>{item.title}</div>
+      ))}
+    </div>
+  </>
 )
 }
