b7c1c5217f04690f213e74083b90d3f5e7d6f4c8
Dmi Pro <dprotascik@thetrask.com>
2023-07-02 18:26:50 +0200

    images for title were added

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,9 @@ export default function App(){
   <div className="row">
     <div className="col-lg-12 col-md-12 mb-12 text-center">
       <div className="title">
+      <img className="logo-left" src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg" alt="HTML5 Logo" />
         <h1>PRODUCTS</h1>
+        <img className="logo-right" src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg" alt="HTML5 Logo" />
       </div>
     </div>
     {products.map((item, index) => (
