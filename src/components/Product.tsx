c08d3a9148bbaf63f476c89943ccecc176c156ec
Dmi Pro <dprotascik@thetrask.com>
2023-07-11 17:07:17 +0200

    added filters, fixed

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -3,15 +3,17 @@ type ProductProps = {
   title: string;
   description: string;
   image: string;
+  price: number;
 };
 
 export const Product = (props: ProductProps) => {
   return (
-    <div className="col-lg-4 col-md-6 mb-4" key={props.productId}>
+    <div className="col-lg-4 col-md-6 mb-4">
       <div className="card h-100">
         <div className="card-image" style={{ backgroundImage: `url(${props.image})` }}></div>
         <div className="card-body">
           <h4 className="card-title">{props.title}</h4>
+          <h3 className="card-price">${props.price}</h3>
           <h6 className="card-description">{props.description}</h6>
         </div>
       </div>
--- src/components/Productlist.tsx
+++ src/components/Productlist.tsx
@@ -1,28 +1,103 @@
 import { useEffect, useState } from "react";
-import { Product } from "./Product"
+import { Product } from "./Product";
+
+type ProductRes = {
+    products: ProductType[];
+}
+
+type ProductType = {
+    id: number
+    title: string
+    description: string;
+    images: string[];
+    price: number;
+}
+
+const add = (a: number, b: number): number => a + b
 
 export const ProductList = () => {
 
-    const [products, setProducts] = useState<Product[]>([]);
+    const [products, setProducts] = useState<ProductType[]>([]);
 
 
     useEffect(() => {
         fetch('https://dummyjson.com/products')
             .then(res => res.json())
             .then(res => {
-                console.log(res.products);
-                setProducts(res.products);
+                const data = res as ProductRes
+                console.log(data.products);
+
+                setProducts(data.products);
+
             });
     }, []);
 
+    const filterProducts = () => {
+        console.log("filtered")
+        setProducts((prevState) => {
+            const result: ProductType[] = []
+            for (let i = 0; i < prevState.length; i++) {
+                if (prevState[i].price < 100) {
+                    result.push(prevState[i])
+                }
+            }
+            return result
+
+        });
+
+    }
+
+    const filterProducts2 = () => {
+        console.log("filtered2")
+        setProducts((prevState) => {
+            const result: ProductType[] = []
+            for (const product of prevState) {
+                if (product.price < 100) {
+                    result.push(product)
+                }
+            }
+
+            return result
+
+        });
+
+    }
+
+    const filterProducts3 = () => {
+        console.log("filtered3")
+        setProducts((prevState) => {
+            const result: ProductType[] = []
+            prevState.forEach((product)=>{
+                if(product.price < 100){
+                    result.push(product)
+                }
+            })
+
+            return result
+
+        });
+
+    }
+
+
     return (
         <>
-            {products.map((item) => (
-                <Product title={item.title}
-                 description={item.description}
-                  key={item.id}
-                   image={item.images[0]} />
-            ))}
+            <div>
+                <button onClick={() => filterProducts3()} type="button">Filter under 100</button>
+
+            </div>
+            <div>
+                {products.map((item) => (
+                    <Product
+                        key={item.id}
+                        title={item.title}
+                        description={item.description}
+                        productId={item.id}
+                        image={item.images[0]}
+                        price={item.price}
+                    />
+                ))}
+            </div>
         </>
     );
 }
